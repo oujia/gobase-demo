ae96675e87bce6a2687cb83b723c540322821857
solupro <sendsoluex@gmail.com>
2018-05-27 18:39:07 +0800

    getOne test

--- controller/SomeController.go
+++ controller/SomeController.go
@@ -16,17 +16,38 @@ func init()  {
 }
 
 func EpisodeTest(c *gin.Context)  {
-	var id int
+	ids := make([]int, 0)
 	keyword := map[string]interface{}{
+		"_foundRows": true,
 		"_field": "id",
 		"_sort": "id desc",
 		"_limit": 5,
+		"_where": "id>200000",
 	}
-	err := episodeDao.GetOne(&id, nil, keyword)
+	err := episodeDao.GetCol(&ids, nil, keyword)
 	if err != nil {
 		gobase.NewResponse(gobase.CODE_DB_ERROR, err.Error()).SendBy(c)
 		return
 	}
 
-	gobase.NewResponse(gobase.CODE_SUCCESS, id).SendBy(c)
+	total := episodeDao.GetFoundRows()
+
+	var hash string
+	where := map[string]interface{} {
+		"id": 205979,
+	}
+	keyword = map[string]interface{}{
+		"_field": "hash",
+	}
+	err = episodeDao.GetOne(&hash, where, keyword)
+	if err != nil {
+		gobase.NewResponse(gobase.CODE_DB_ERROR, err.Error()).SendBy(c)
+		return
+	}
+
+	gobase.NewResponse(gobase.CODE_SUCCESS, map[string]interface{} {
+		"idList": ids,
+		"hashOne": hash,
+		"total": total,
+	}).SendBy(c)
 }
